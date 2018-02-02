3e29641d1423a77e1eeb4c5bc2a9b46341ede361
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-01 22:14:50 -0600

    Agregando ensamblado de controles

---
+++ AguaSB.Views.Controles/LogoAnimado.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AguaSB.Views.Controles
+{
+    public partial class LogoAnimado : UserControl
+    {
+        public LogoAnimado()
+        {
+            InitializeComponent();
+        }
+    }
+}
