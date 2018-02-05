f327f9fb87d45de3495a3e98f44ac863174dc400
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-05 15:16:00 -0600

    Agregando conversor BoolToVisibility

--- AguaSB.Views.Conversores/ArithmeticConverter.cs
+++ AguaSB.Views.Conversores/ArithmeticConverter.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace AguaSB.Views.Conversores
 {
-    public class ArithmeticConverter
+    public class ArithmeticConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrWhiteSpace(parameter as string))
+                throw new ArgumentNullException(nameof(parameter));
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
     }
 }
