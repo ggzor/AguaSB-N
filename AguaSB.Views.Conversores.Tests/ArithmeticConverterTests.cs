1a1dc98e471752538c297d68f3c272e95f461f33
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-05 16:00:27 -0600

    Agregando pruebas básicas para conversor aritmético

--- AguaSB.Views.Conversores.Tests/ArithmeticConverterTests.cs
+++ AguaSB.Views.Conversores.Tests/ArithmeticConverterTests.cs
@@ -1,10 +1,23 @@
 ﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
 
 namespace AguaSB.Views.Conversores.Tests
 {
     [TestFixture]
     public class ArithmeticConverterTests
     {
-        
+        private readonly Fixture Any = new Fixture();
+        private readonly ArithmeticConverter Converter = new ArithmeticConverter();
+
+        [Test]
+        public void ShouldThrowArgumentNullException_WhenParameterIsNullOrWhitespace() =>
+            Assert.Throws<ArgumentNullException>(() => Converter.Convert(Any.Create<int>(), null, "", null));
+
+        [Test]
+        public void ShouldThrowArgumentException_When_AOrParameterReference_IsNullOrWhitespace()
+        {
+            //Assert.Throws<ArgumentException>(() => Converter.Convert(Any.Create<int>))
+        }
     }
 }
