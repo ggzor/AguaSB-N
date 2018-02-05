a2a958180eea7355138704b3ee948ff67303d115
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-05 11:38:41 -0600

    Reestructurando jerarquía de extensiones

--- AguaSB.Extensiones.Views/IExtensionView.cs
+++ AguaSB.Extensiones.Views/IExtensionView.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AguaSB.Extensiones.Views
+﻿namespace AguaSB.Extensiones.Views
 {
-    /// <summary>
-    /// Nota: No implementar <see cref="IExtension"/> y esta interfaz juntos.
-    /// </summary>
-    public interface IExtensionView : IVersionable
+    public interface IExtensionView : IExtension
     {
 
     }
--- AguaSB.Extensiones/IVersionable.cs
+++ AguaSB.Extensiones/IVersionable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AguaSB.Extensiones
+﻿namespace AguaSB.Extensiones
 {
     public interface IVersionable
     {
