32e2e595cf537a15175cb96b28e865f54ecf3d25
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-05 19:58:15 -0600

    Agregando conversores reactivos

--- AguaSB.Views.Conversores/BoolToVisibilityConverter.cs
+++ AguaSB.Views.Conversores/BoolToVisibilityConverter.cs
@@ -5,7 +5,7 @@ using System.Windows.Data;
 
 namespace AguaSB.Views.Conversores
 {
-    public class BoolToVisibility : IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
---
+++ AguaSB.Views.Conversores/Reactive/BoolToVisibility.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AguaSB.Views.Conversores.Reactive
+{
+    public static class BoolToVisibility
+    {
+        public static readonly BoolToVisibilityConverter Converter = new BoolToVisibilityConverter();
+
+        public static Visibility Convert(bool value) => (Visibility)Converter.Convert(value, typeof(Visibility), null, null);
+    }
+}
