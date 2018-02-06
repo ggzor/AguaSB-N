26a0bab6e1345d6ab78f264a3cdac73ae823e10e
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-05 20:21:54 -0600

    Agregando utilerías para incrementar usabilidad de cajas de texto

---
+++ AguaSB.Views.Utilerias/PasswordBoxUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Views.Utilerias
+{
+    public static class PasswordBoxUtil
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty =
+            DependencyProperty.RegisterAttached("SelectAllOnFocus", typeof(bool), typeof(PasswordBoxUtil),
+                new PropertyMetadata(false, (s, a) => TrySet((PasswordBox)s, (bool)a.NewValue)));
+
+        public static void SetSelectAllOnFocus(PasswordBox elem, bool val) => elem.SetValue(SelectAllOnFocusProperty, val);
+        public static bool GetSelectAllOnFocus(PasswordBox elem) => (bool)elem.GetValue(SelectAllOnFocusProperty);
+
+        private static void TrySet(PasswordBox textBox, bool value)
+        {
+            if (value)
+            {
+                textBox.GotKeyboardFocus += async (s, a) =>
+                {
+                    await Task.Delay(20).ConfigureAwait(true); // Quite ugly but required to make it work
+                    textBox.SelectAll();
+                };
+            }
+        }
+    }
+}
---
+++ AguaSB.Views.Utilerias/TextFieldUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace AguaSB.Views.Utilerias
+{
+    public static class TextFieldUtil
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty =
+            DependencyProperty.RegisterAttached("SelectAllOnFocus", typeof(bool), typeof(TextFieldUtil),
+                new PropertyMetadata(false, (s, a) => TrySet((TextBoxBase)s, (bool)a.NewValue)));
+
+        public static void SetSelectAllOnFocus(TextBoxBase elem, bool val) => elem.SetValue(SelectAllOnFocusProperty, val);
+        public static bool GetSelectAllOnFocus(TextBoxBase elem) => (bool)elem.GetValue(SelectAllOnFocusProperty);
+
+        private static void TrySet(TextBoxBase textBox, bool value)
+        {
+            if (value)
+            {
+                textBox.GotKeyboardFocus += async (s, a) =>
+               {
+                   await Task.Delay(20).ConfigureAwait(true); // Quite ugly but required to make it work
+                    textBox.SelectAll();
+               };
+            }
+        }
+    }
+}
