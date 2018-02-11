cd975b2bccd3fbbbc15853b9034be69cee7a2928
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-10 23:56:55 -0600

    Agregando implementación de carga de extensiones autorizadas y pruebas unitarias

---
+++ AguaSB.Individual.Pagos.Tests/CargadorExtensionesAutorizadasTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+using NSubstitute;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+
+using AguaSB.Compartido.Interfaces;
+using AguaSB.Extensiones;
+using AguaSB.Autenticacion;
+
+namespace AguaSB.Individual.Pagos.Tests
+{
+    [TestFixture]
+    public class CargadorExtensionesAutorizadasTests
+    {
+        private readonly Fixture Cualquiera = new Fixture();
+
+        [SetUp]
+        public void Inicializar()
+        {
+            Cualquiera.Customize(new AutoNSubstituteCustomization());
+        }
+
+        [Test]
+        public void DeberiaDevolverExtensionesAutorizadasParaSesion_CuandoExtensionEsValidaParaSesion()
+        {
+            // Arrange
+            var extensionesAutorizadas = Cualquiera.CreateMany<IExtension>();
+            var todasExtensiones = extensionesAutorizadas.Concat(Cualquiera.CreateMany<IExtension>());
+
+            var cargadorExtensiones = Substitute.For<ICargadorExtensiones>();
+            var autorizadorExtensiones = Substitute.For<IAutorizadorExtension>();
+
+            var agregadoTodasExtensiones = new AgregadoExtensiones();
+            agregadoTodasExtensiones.Registrar(todasExtensiones);
+
+            cargadorExtensiones.Cargar(null).ReturnsForAnyArgs(agregadoTodasExtensiones);
+
+            autorizadorExtensiones.Autorizar(null, null).ReturnsForAnyArgs(ci =>
+            {
+                if (extensionesAutorizadas.Contains(ci.Arg<IExtension>()))
+                    return new AutorizacionExitosa();
+                else
+                    return new AutorizacionFallida();
+            });
+
+            var cargadorExtensionesAutorizadas = new CargadorExtensionesAutorizadas(cargadorExtensiones, autorizadorExtensiones);
+
+            // Act
+            var extensionesCargadas = cargadorExtensionesAutorizadas.Cargar(Cualquiera.Create<Sesion>());
+
+            // Assert
+            CollectionAssert.AreEquivalent(extensionesAutorizadas, extensionesCargadas.Todas);
+        }
+    }
+}
---
+++ AguaSB.Individual.Pagos/CargadorExtensionesAutorizadas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+using AguaSB.Autenticacion;
+using AguaSB.Compartido.Interfaces;
+using AguaSB.Extensiones;
+
+namespace AguaSB.Individual.Pagos
+{
+    public class CargadorExtensionesAutorizadas : ICargadorExtensiones
+    {
+        public IAutorizadorExtension AutorizadorExtension { get; }
+        public ICargadorExtensiones CargadorExtensiones { get; }
+
+        public CargadorExtensionesAutorizadas(ICargadorExtensiones cargadorExtensiones, IAutorizadorExtension autorizadorExtension)
+        {
+            AutorizadorExtension = autorizadorExtension ?? throw new ArgumentNullException(nameof(autorizadorExtension));
+            CargadorExtensiones = cargadorExtensiones ?? throw new ArgumentNullException(nameof(cargadorExtensiones));
+        }
+
+        public AgregadoExtensiones Cargar(Sesion sesion) =>
+            new AgregadoExtensiones(
+                from extension in CargadorExtensiones.Cargar(sesion)
+                let resultadoAutorizacion = AutorizadorExtension.Autorizar(extension, sesion)
+                where resultadoAutorizacion is AutorizacionExitosa
+                select extension
+            );
+    }
+}
