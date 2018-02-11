7d3ba9e64e9370ab4f63827781a2cfacbd413d5e
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-10 23:52:26 -0600

    Agregando interfaz de cargador de extensiones

---
+++ AguaSB.Compartido.Interfaces/ICargadorExtensiones.cs
@@ -0,0 +1,10 @@
+﻿using AguaSB.Autenticacion;
+using AguaSB.Extensiones;
+
+namespace AguaSB.Compartido.Interfaces
+{
+    public interface ICargadorExtensiones
+    {
+        AgregadoExtensiones Cargar(Sesion sesion);
+    }
+}
