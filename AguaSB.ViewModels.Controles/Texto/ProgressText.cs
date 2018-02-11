c1d84d8064405929af62de4031ddeacd9177a92f
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-10 20:55:59 -0600

    Extrayendo lógica y visualización de textos que indican progreso

---
+++ AguaSB.ViewModels.Controles/Texto/ProgressText.cs
@@ -0,0 +1,37 @@
+﻿using ReactiveUI;
+
+namespace AguaSB.ViewModels.Controles
+{
+    public class ProgressText : ReactiveObject
+    {
+        public ProgressText(string initialTitle = "", string initialSubtitle = "")
+        {
+            title = initialTitle ?? string.Empty;
+            subtitle = initialSubtitle ?? string.Empty;
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { this.RaiseAndSetIfChanged(ref title, value); }
+        }
+
+        private string subtitle;
+
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { this.RaiseAndSetIfChanged(ref subtitle, value); }
+        }
+
+        public void Set(string title, string subtitle = "")
+        {
+            Title = title ?? string.Empty;
+            Subtitle = subtitle ?? string.Empty;
+        }
+
+        public void Clear() => Title = Subtitle = string.Empty;
+    }
+}
---
+++ AguaSB.Views.Controles/Texto/ProgressTextView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using ReactiveUI;
+
+using AguaSB.ViewModels.Controles;
+using System.Reactive.Disposables;
+
+namespace AguaSB.Views.Controles.Texto
+{
+    public partial class ProgressTextView : UserControl, IViewFor<ProgressText>
+    {
+        public ProgressTextView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Title, v => v.Title.Text).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Subtitle, v => v.Subtitle.Text).DisposeWith(d);
+            });
+        }
+
+        public Style TitleStyle
+        {
+            get { return (Style)GetValue(TitleStyleProperty); }
+            set { SetValue(TitleStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleStyleProperty =
+            DependencyProperty.Register(nameof(TitleStyle), typeof(Style), typeof(ProgressTextView), new PropertyMetadata(null));
+
+        public Style SubtitleStyle
+        {
+            get { return (Style)GetValue(SubtitleStyleProperty); }
+            set { SetValue(SubtitleStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubtitleStyleProperty =
+            DependencyProperty.Register(nameof(SubtitleStyle), typeof(Style), typeof(ProgressTextView), new PropertyMetadata(null));
+
+        #region IViewFor
+        public ProgressText ViewModel
+        {
+            get { return (ProgressText)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(nameof(ViewModel), typeof(ProgressText), typeof(ProgressTextView), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (ProgressText)value;
+        }
+        #endregion
+    }
+}
