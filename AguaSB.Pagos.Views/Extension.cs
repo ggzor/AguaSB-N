3c9b6a3d80ccb8a9407b2625b129279c6ea40105
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-10 19:22:24 -0600

    Agregando extensión de pagos

---
+++ AguaSB.Pagos.Views/Extension.cs
@@ -0,0 +1,20 @@
+﻿using AguaSB.Extensiones.Views;
+using AguaSB.Views;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Pagos.Views
+{
+    public class Extension : IExtensionView
+    {
+        public IView View { get; }
+
+        public FrameworkElement Icono => new Button { Content = "Pagos" };
+
+        public Extension(Principal principal)
+        {
+            View = principal;
+        }
+    }
+}
---
+++ AguaSB.Pagos.Views/Principal.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AguaSB.Views;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AguaSB.Pagos.Views
+{
+    public partial class Principal : UserControl, IView
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        public FrameworkElement View => this;
+
+        public void Entrar()
+        {
+
+        }
+    }
+}
