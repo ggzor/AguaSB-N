8572358bc7d96c9aee33d1a83f8cef2504d329b4
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-10 21:18:35 -0600

    Agregando colección de extensiones fuertemente tipeado

---
+++ AguaSB.Extensiones/AgregadoExtensiones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AguaSB.Extensiones
+{
+    public sealed class AgregadoExtensiones
+    {
+        private readonly Dictionary<Type, IEnumerable> Registro = new Dictionary<Type, IEnumerable>();
+
+        public void Registrar<T>(IEnumerable<T> extensiones) where T : IExtension =>
+            Registro.Add(typeof(T), extensiones);
+
+        public IEnumerable<T> Obtener<T>() where T : IExtension =>
+            Registro[typeof(T)].OfType<T>();
+    }
+}
