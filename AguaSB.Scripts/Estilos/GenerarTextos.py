bdd5a2211dc1b7b089c12f228be8ef0092246db4
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-16 20:08:12 -0600

    Agregando scripts para generación de estilos

---
+++ AguaSB.Scripts/Estilos/GenerarTextos.py
@@ -0,0 +1,22 @@
+﻿plantilla = """
+\t<Style x:Key="{tono}-{peso}{tamaño}" TargetType="TextBlock" BasedOn="{{StaticResource {tono}-{peso}}}">
+\t\t<Setter Property="FontSize" Value="{{StaticResource t{tamaño}}}"/>
+\t</Style>"""
+
+def crearCodigo(tono, peso, tamaño):
+    return plantilla.format(**locals())
+
+def generar(tonos, pesos, tamaños):
+    for t in tonos:
+        for p in pesos:
+            for h in tamaños:
+                yield crearCodigo(t, p, h)
+
+tonos = ["w", "b"]
+pesos = ["l", "n", "h", "u"]
+tamaños = list(map(str, range(1, 7)))
+
+resultado = list(generar(tonos, pesos, tamaños))
+
+for r in resultado:
+    print(r)
