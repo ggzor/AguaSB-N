2429ea5bd876e27c95d83b23e4789ce1035b55a5
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-17 21:35:52 -0600

    Implementando API de navegación en Pagos

--- AguaSB.Pagos.Views/Extension.cs
+++ AguaSB.Pagos.Views/Extension.cs
@@ -1,15 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 using MahApps.Metro.IconPacks;
 
 using AguaSB.Extensiones.Views;
 using AguaSB.Extensiones.Views.Menu;
 using AguaSB.Views;
+using AguaSB.Views.Navegacion;
 
 namespace AguaSB.Pagos.Views
 {
     public class Extension : IExtensionMenu
     {
+        public IObservable<IClaveNavegacion> Navegacion => throw new NotImplementedException();
+
         public IView View { get; }
 
         public FrameworkElement Icono => IconoMenuEstatico.Crear(PackIconFontAwesomeKind.DollarSignSolid);
