64138d93842ba0ee3e8c13bd88e64dd52b4b0c47
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-19 11:10:39 -0600

    Agregando eventos a selección de extensión del menú

--- AguaSB.Extensiones.Views/Menu/ExtensionMenuView.xaml.cs
+++ AguaSB.Extensiones.Views/Menu/ExtensionMenuView.xaml.cs
@@ -1,15 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace AguaSB.Extensiones.Views.Menu
 {
     public partial class ExtensionMenuView : UserControl
     {
-
+        public IObservable<ExtensionMenuView> Seleccionada { get; }
 
         public ExtensionMenuView()
         {
             InitializeComponent();
+            Seleccionada = Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(e => Boton.Click += e, e => Boton.Click -= e)
+                .Select(e => (ExtensionMenuView)e.Sender);
         }
 
         public IExtensionMenu Extension
