bfe6c14caf585f4a34319be13716618decd60287
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-25 22:44:43 -0600

    Agregando clases para aparición y desaparición de componentes.

---
+++ AguaSB.Views.Animaciones/GenericFader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+using AguaSB.Views.Animaciones.Pipelines;
+
+namespace AguaSB.Views.Animaciones
+{
+    internal class GenericFader : IFader
+    {
+        public Duration Duration { get; }
+        public TimeSpan? Delay { get; }
+        public IEasingFunction Easing { get; }
+
+        public Visibility InitialVisibility { get; }
+        public double InitialOpacity { get; }
+
+        public Visibility TargetVisibility { get; }
+        public double TargetOpacity { get; }
+
+        public GenericFader(Duration duration, TimeSpan? delay, IEasingFunction easing,
+            Visibility initialVisibility, double initialOpacity, Visibility targetVisibility, double targetOpacity)
+        {
+            Duration = duration;
+            Delay = delay;
+            Easing = easing;
+            InitialVisibility = initialVisibility;
+            InitialOpacity = initialOpacity;
+            TargetVisibility = targetVisibility;
+            TargetOpacity = targetOpacity;
+        }
+
+        public IFutureAnimation Create(FrameworkElement element)
+        {
+            void SetUpElement()
+            {
+                element.Visibility = InitialVisibility;
+                element.Opacity = InitialOpacity;
+            }
+
+            var animation = new DoubleAnimation
+            {
+                BeginTime = Delay,
+                Duration = Duration,
+                EasingFunction = Easing,
+                To = TargetOpacity
+            };
+
+            void PostAction() => element.Visibility = TargetVisibility;
+
+            return new FutureAnimation(
+                preAction: SetUpElement,
+                timeline: animation,
+                postAction: PostAction);
+        }
+
+        public void Apply(FrameworkElement element) =>
+            Create(element).BeginWith(element);
+    }
+}
---
+++ AguaSB.Views.Animaciones/IFader.cs
@@ -0,0 +1,18 @@
+﻿using AguaSB.Views.Animaciones.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace AguaSB.Views.Animaciones
+{
+
+    public interface IFader
+    {
+        void Apply(FrameworkElement element);
+        IFutureAnimation Create(FrameworkElement element);
+    }
+}
