f2a0de3a9c8132413249935d05545abbbddf238b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-03-18 00:07:58 -0600

    Integrando animaciones de Project Neon

--- AguaSB.Individual.Pagos/Views/NavegadorViews.cs
+++ AguaSB.Individual.Pagos/Views/NavegadorViews.cs
@@ -2,11 +2,10 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Animation;
 
 using AguaSB.Views;
 using AguaSB.Views.Animaciones;
-using AguaSB.Views.Animaciones.Definition;
+using AguaSB.Views.Animaciones.Neon;
 
 namespace AguaSB.Individual.Pagos.Views
 {
@@ -19,57 +18,6 @@ namespace AguaSB.Individual.Pagos.Views
             Panel = panel ?? throw new ArgumentNullException(nameof(panel));
         }
 
-        #region Configuracion
-        private static readonly IEasingFunction Easing = new CircleEase { EasingMode = EasingMode.EaseOut };
-        private const double EscalaFrontal = 0.85;
-        private const double EscalaTrasero = 1.1;
-
-        private static readonly ISingleElementAnimation AnimacionesAtrasTrasero = new ISingleElementAnimation[]
-        {
-            Scale.Uniform
-                .WithFromTo(EscalaTrasero, 1.0)
-                .WithDuration(TimeSpan.FromMilliseconds(400))
-                .WithEasing(Easing),
-            Fade.In
-                .WithDuration(TimeSpan.FromMilliseconds(200))
-                .WithEasing(Easing)
-        }.Compose();
-
-        private static readonly ISingleElementAnimation AnimacionesAtrasFrontal = new ISingleElementAnimation[]
-        {
-            Scale.Uniform
-                .WithFromTo(1.0, EscalaFrontal)
-                .WithDuration(TimeSpan.FromMilliseconds(200))
-                .WithEasing(Easing),
-            Fade.Out
-                .WithDuration(TimeSpan.FromMilliseconds(200))
-                .WithEasing(Easing)
-        }.Compose();
-
-        private static readonly ISingleElementAnimation AnimacionesAdelanteTrasero = new ISingleElementAnimation[]
-        {
-            Fade.Out
-                .WithDuration(TimeSpan.FromMilliseconds(100))
-                .WithEasing(Easing),
-            Scale.Uniform
-                .WithFromTo(1, EscalaTrasero)
-                .WithDuration(TimeSpan.FromMilliseconds(400))
-                .WithEasing(Easing)
-        }.Compose();
-
-        private static readonly ISingleElementAnimation AnimacionesAdelanteFrontal = new ISingleElementAnimation[]
-        {
-            Fade.In
-                .WithDuration(TimeSpan.FromMilliseconds(200))
-                .WithEasing(Easing)
-                .WithFrom(0.2),
-            Scale.Uniform
-                .WithFromTo(EscalaFrontal, 1)
-                .WithEasing(Easing)
-                .WithDuration(TimeSpan.FromMilliseconds(400))
-        }.Compose();
-        #endregion
-
         public void Atras(IView view, object parametro)
         {
             if (view == null)
@@ -78,16 +26,15 @@ namespace AguaSB.Individual.Pagos.Views
             var trasero = view.View;
             var frontal = Panel.Children.OfType<FrameworkElement>().LastOrDefault();
 
-            var animacionTrasero = AnimacionesAtrasTrasero
+            var animacionTrasero = Appear.FromAbove
                 .Create(trasero)
-                .Before(() => Panel.Children.Insert(Panel.Children.Count - 1, trasero))
-                .Then(() => view.Entrar(parametro));
+                .Before(() => Panel.Children.Insert(Panel.Children.Count - 1, trasero));
 
             var animacionFrontal = frontal == null
                 ? FutureAnimation.NoAnimation
-                : AnimacionesAtrasFrontal.Create(frontal).Then(() => Panel.Children.Remove(frontal));
+                : Disappear.ToBelow.Create(frontal).Then(() => Panel.Children.Remove(frontal));
 
-            animacionTrasero.Pair(animacionFrontal)
+            animacionTrasero.Pair(animacionFrontal, CrearRetrasoEntrada(view, parametro))
                 .BeginIn(Panel);
         }
 
@@ -101,15 +48,17 @@ namespace AguaSB.Individual.Pagos.Views
 
             var animacionTrasero = trasero == null
                 ? FutureAnimation.NoAnimation
-                : AnimacionesAdelanteTrasero.Create(trasero).Then(() => Panel.Children.Remove(trasero));
+                : Disappear.ToAbove.Create(trasero).Then(() => Panel.Children.Remove(trasero));
 
-            var animacionFrontal = AnimacionesAdelanteFrontal
+            var animacionFrontal = Appear.FromBelow
                 .Create(frontal)
-                .Before(() => Panel.Children.Add(frontal))
-                .Then(() => view.Entrar(parametro));
+                .Before(() => Panel.Children.Add(frontal));
 
-            animacionTrasero.Pair(animacionFrontal)
+            animacionTrasero.Pair(animacionFrontal, CrearRetrasoEntrada(view, parametro))
                 .BeginIn(Panel);
         }
+
+        private static IFutureAnimation CrearRetrasoEntrada(IView view, object parametro) =>
+            Delay.Action.For(TimeSpan.FromMilliseconds(20)).Create(() => view.Entrar(parametro));
     }
 }
